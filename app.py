1ccfba7a356b8ac6b18cbb3ac7211c0655776f96
Madhusudan <madhusudhanpm059@gmail.com>
2026-02-10 22:27:25 -0800

    Testing GitHub integration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>CI/CD Mastery Achieved!</h1><p>Build #4 is 100% Automated.</p>"
+    return "<h1>CI/CD Mastery Achieved!</h1><p>Build #4 is 100% Automated. Lets go do this bro</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
